1bdad5c62aed3225a32202f537c805bc62cfa0f4
Tamara Smiljanic <babictamara@yahoo.com>
2020-05-21 18:44:15 +0200

    added HomePage class

--- PageObjectModel.cs
+++ PageObjectModel.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cas31SeleniumTests.PageObjects;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
+
 namespace Cas31SeleniumTests
 {
     class PageObjectModel
     {
-        IWebDriver driver;
-        WebDriverWait wait;
-
+        private IWebDriver driver;
+        
         [SetUp]
         public void SetUp()
         {
-            driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
         }
 
         [TearDown]
         public void TearDown()
         {
-            driver.Close();
+            this.driver.Close();
+        }
+
+        [Test]
+        public void TestGoogleSearch()
+        {
+            HomePage naslovna = new HomePage(this.driver);
+            naslovna.GoToPage();
         }
     }
 }
---
+++ PageObjects/HomePage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Cas31SeleniumTests.PageObjects
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, new TimeSpan(0,0,70));
+        }
+
+        public void GoToPage()
+        {
+            this.driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+    }
+}
